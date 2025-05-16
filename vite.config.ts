d8606f5e4ab3bef87431b0f77692c6359851731e
Ali Sharifi <68825406+alisharifi963@users.noreply.github.com>
2025-05-16 18:33:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["ethers"], // برای جلوگیری از باندل کردن ethers
+      external: ["ethers"], 
     },
   },
 });
